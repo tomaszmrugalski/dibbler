fcda57e6fb3228d4f875ded1efdcd972f1be2f5c
Tomek Mrugalski <thomson@klub.com.pl>
2004-01-26 23:03:55 +0000

    *** empty log message ***

---
+++ IfaceMgr/test1.cpp
@@ -0,0 +1,11 @@
+#include "IfaceMgr.h"
+
+using namespace std;
+int main() {
+    TIfaceMgr mgr;
+
+    cout << mgr << endl;
+//    ipaddr_add("eth0", 2, "2000::1234", 100, 200);
+
+    sleep(100);
+}
