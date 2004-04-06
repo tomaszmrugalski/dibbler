1aa0a34d8368d1722a27ca42eaa4e7d88ab51283
Tomek Mrugalski <thomson@klub.com.pl>
2004-04-06 22:18:54 +0000

    *** empty log message ***

--- AddrMgr/AddrMgr.cpp
+++ AddrMgr/AddrMgr.cpp
@@ -6,9 +6,12 @@
  *
  * released under GNU GPL v2 or later licence
  *
- * $Id: AddrMgr.cpp,v 1.4 2004-04-06 21:57:34 thomson Exp $
+ * $Id: AddrMgr.cpp,v 1.5 2004-04-06 22:18:54 thomson Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2004/04/06 21:57:34  thomson
+ * *** empty log message ***
+ *
  * Revision 1.3  2004/03/29 18:53:08  thomson
  * Author/Licence/cvs log/cvs version headers added.
  *
@@ -46,11 +49,13 @@ void TAddrMgr::dbStore()
 {
     char buf[128];
     ofstream f;
+//    return;
     strcpy(buf,this->dbfile.c_str());
-    f.open(buf);
-    f << *this;
-    f.close();
-//  std::clog << logger::logDebug << "AddrDB saved in " << this->dbfile << logger::endl;
+    std::clog << "###dbStore(): " << buf << std::endl;
+
+//    f.open(buf);
+//    f << *this;
+//    f.close();
 }
 
 void TAddrMgr::addClient(SmartPtr<TAddrClient> x)
