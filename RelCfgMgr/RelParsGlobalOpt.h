224240a10a81a331148c9dbfe42e5378829ec20d
Tomek Mrugalski <thomson@klub.com.pl>
2005-07-16 14:46:32 +0000

    Compatilibility with gcc 2.95 improved (bug #119) Fix was provided by Tomasz Torcz. Thanks.

--- RelCfgMgr/RelParsGlobalOpt.h
+++ RelCfgMgr/RelParsGlobalOpt.h
@@ -6,14 +6,18 @@
  *
  * released under GNU GPL v2 or later licence
  *
- * $Id: RelParsGlobalOpt.h,v 1.1 2005-01-11 22:53:35 thomson Exp $
+ * $Id: RelParsGlobalOpt.h,v 1.2 2005-07-16 14:46:32 thomson Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/01/11 22:53:35  thomson
+ * Relay skeleton implemented.
+ *
  */
 
 #ifndef TRELPARSGLOBALOPT_H_
 #define TRELPARSGLOBALOPT_H_
 #include "RelParsIfaceOpt.h"
+#include <string>
 
 class TRelParsGlobalOpt : public TRelParsIfaceOpt
 {
