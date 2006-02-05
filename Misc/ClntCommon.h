a022bd29e51fa329e51b15cb896b23dcb39bb32c
Tomek Mrugalski <thomson@klub.com.pl>
2006-02-05 23:44:22 +0000

    *** empty log message ***

---
+++ Misc/ClntCommon.h
@@ -0,0 +1,31 @@
+/*
+ * Dibbler - a portable DHCPv6
+ *
+ * authors: Tomasz Mrugalski <thomson@klub.com.pl>
+ *          Marek Senderski <msend@o2.pl>
+ *
+ * Released under GNU GPL v2 licence
+ *
+ * $Id: ClntCommon.h,v 1.1.2.1 2006-02-05 23:44:22 thomson Exp $
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/01/11 22:53:35  thomson
+ * Relay skeleton implemented.
+ *
+ */
+
+class TClntIfaceMgr;
+class TClntCfgMgr;
+class TClntTransMgr;
+class TClntAddrMgr;
+#ifndef CLNTCOMMON_H
+#define CLNTCOMMON_H
+
+struct Ctx_s {
+    TClntIfaceMgr * IfaceMgr;
+    TClntCfgMgr   * CfgMgr;
+    TClntTransMgr * TransMgr;
+};
+typedef struct Ctx_s TCtx;
+
+#endif /* CLNTCOMMON_H */
