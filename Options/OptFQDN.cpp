a521df77ffffcb155d201ff1d0d0b7b06312127c
Tomek Mrugalski <thomson@klub.com.pl>
2006-03-03 20:37:55 +0000

    *** empty log message ***

---
+++ Options/OptFQDN.cpp
@@ -0,0 +1,158 @@
+/*
+* Dibbler - a portable DHCPv6
+*
+* authors: Adrien CLERC, Bahattin DEMIRPLAK, Gaëtant ELEOUET
+*          Mickaël GUÉRIN, Lionel GUILMIN, Lauréline PROVOST
+*          from ENSEEIHT, Toulouse, France
+*
+* released under GNU GPL v2 licence
+*
+* $Id: OptFQDN.cpp,v 1.1 2006-03-03 20:37:55 thomson Exp $
+*
+*/
+
+#ifdef LINUX
+#include <netinet/in.h>
+#endif
+#ifdef WIN32
+#include <winsock2.h>
+#endif
+
+#include "OptFQDN.h"
+#include "Logger.h"
+
+TOptFQDN::TOptFQDN(string fqdn, TMsg* parent)
+		:TOpt(OPTION_FQDN, parent) {
+	this->fqdn = fqdn;
+	flag_N = false;
+	flag_S = false;
+	// This flag is always off in client messages.
+	this->flag_O = false;
+
+	this->Valid = true;
+}
+
+
+TOptFQDN::TOptFQDN(char * &buf, int &bufsize, TMsg* parent)
+		:TOpt(OPTION_FQDN, parent) {
+    this->Valid = false;
+    // Extracting flags...
+    //TODO some checks from flags.
+    char flags = *buf;
+    this->flag_N = flags && FQDN_N;
+    this->flag_S = flags && FQDN_S;
+    this->flag_O = flags && FQDN_O;
+    buf += 1;
+    bufsize -= 1;
+
+    //Extracting domain name
+    fqdn = "";
+    if ( bufsize <= 255 ) {
+	short tmplength = *buf;
+	buf++;
+	while (tmplength != 0) {
+	    fqdn.append(buf, tmplength);
+	    buf += tmplength;
+	    bufsize -= tmplength;
+	    tmplength = *buf;
+	    buf++;
+	    if ( tmplength != 0 ) {
+		fqdn.append(".");
+	    }
+	}
+	buf++;
+	bufsize--;
+	Valid = true;
+    }
+
+}
+
+TOptFQDN::~TOptFQDN() {
+	return;
+}
+
+void TOptFQDN::setNFlag(bool flag) {
+	flag_N = flag;
+}
+
+void TOptFQDN::setSFlag(bool flag) {
+	flag_S = flag;
+}
+
+void TOptFQDN::setOFlag(bool flag) {
+	flag_O = flag;
+}
+
+string TOptFQDN::getFQDN() {
+	return fqdn;
+}
+
+int TOptFQDN::getSize() {
+	//Each point will be removed from the string, and replaced with a length < 63
+	//The first length and the final 0 will increased the fqdn string length by 2
+	//We also have to add 4 for the header (option type and size) and 1 for the flags.
+	// 2 + 1 + 4 = 7
+	return fqdn.length() + 7;
+}
+
+char * TOptFQDN::storeSelf(char *buffer) {
+	// Type and size
+	*(short*)buffer = htons(OptType);
+	buffer+=2;
+	*(short*)buffer = htons(getSize()-4);
+	buffer+=2;
+//Flag Initialization
+	*buffer = 0;
+	if (flag_N) {
+		*buffer += FQDN_N;
+	}
+	if (flag_S) {
+		*buffer += FQDN_S;
+	}
+	if (flag_O) {
+		*buffer += FQDN_O;
+	}
+	buffer++;
+
+	//FQDN data :)
+	if ( fqdn.length() != 0 ) {
+		string copy = "";
+		copy += fqdn;
+		unsigned int dotpos = copy.find('.', 0);
+
+		while(dotpos != string::npos) {
+			*buffer = dotpos;
+			buffer++;
+			memcpy(buffer, copy.c_str(), dotpos);
+			buffer += dotpos;
+			copy.assign(copy, dotpos + 1, copy.length());
+			dotpos = copy.find('.', 0);
+		}
+
+		*buffer = copy.length();
+		buffer++;
+		memcpy(buffer, copy.c_str(), copy.length());
+		buffer += copy.length();
+	}
+	*buffer = 0;
+	buffer++;
+
+	return buffer;
+}
+
+bool TOptFQDN::isValid() {
+	//TODO Check the validity of this option
+	return Valid;
+}
+
+bool TOptFQDN::getNFlag( ) {
+	return flag_N;
+}
+
+bool TOptFQDN::getSFlag( ) {
+	return flag_S;
+}
+
+bool TOptFQDN::getOFlag( ) {
+	return flag_O;
+}
---
+++ Options/OptFQDN.h
@@ -0,0 +1,79 @@
+/*
+* Dibbler - a portable DHCPv6
+*
+* authors: Adrien CLERC, Bahattin DEMIRPLAK, Gaëtant ELEOUET
+*          Mickaël GUÉRIN, Lionel GUILMIN, Lauréline PROVOST
+*          from ENSEEIHT, Toulouse, France
+*
+* released under GNU GPL v2 licence
+*
+* $Id: OptFQDN.h,v 1.1 2006-03-03 20:37:55 thomson Exp $
+*
+*/
+ 
+/**
+ * This class is mother of Client and Server version of the FQDN option.
+ * 
+ * It implements all common methods
+ */
+class TOptFQDN;
+#ifndef OPTFQDN_H
+#define OPTFQDN_H
+
+#include <string>
+#include <pthread.h>
+
+#include "Opt.h"
+#include "DHCPConst.h"
+
+class TOptFQDN : public TOpt
+{
+  public:
+  /**
+   * Constructor
+   * 
+   * The FQDN is set and all flags are 0
+   * @param fqdn The FQDN about to be sent
+   * @param parent The message in which this option is included
+   */
+  	TOptFQDN(string fqdn, TMsg* parent);
+  	
+  	/**
+  	 * Constructor
+  	 * 
+  	 * Build the option with a buffer received
+  	 * @param buf the buffer received, containing the whole option
+  	 * @param bufsize the size of the buffer
+  	 * @param parent the message in which this option is included
+  	 */
+  	TOptFQDN(char * &buf, int &bufsize, TMsg* parent);
+  	
+  	/**
+  	 * Destructor - Does actually nothing
+  	 */
+  	~TOptFQDN();
+  	
+    int getSize();
+    char * storeSelf( char* buf);
+    bool isValid();
+    
+    bool getNFlag();
+    bool getSFlag();
+    bool getOFlag();
+    void setNFlag(bool flag);
+    void setOFlag(bool flag);
+    void setSFlag(bool flag);
+    string getFQDN();
+
+  	
+  private:
+  	string fqdn;
+  	char *encodedFQDN;
+  	bool flag_N;
+  	bool flag_O;
+  	bool flag_S;
+  	bool Valid;
+    
+};
+
+#endif /* OPTFQDN_H */
