915a7a8f2da24e6bd477c1d84fc0016f497a269d
Tomek Mrugalski <thomson@klub.com.pl>
2006-12-28 22:53:00 +0000

    hwType is now printed.

--- SrvIfaceMgr/SrvIfaceIface.cpp
+++ SrvIfaceMgr/SrvIfaceIface.cpp
@@ -6,7 +6,7 @@
  *
  * released under GNU GPL v2 or later licence
  *
- * $Header: /var/cvs/dibbler/SrvIfaceMgr/SrvIfaceIface.cpp,v 1.10 2006-08-22 00:01:21 thomson Exp $
+ * $Header: /var/cvs/dibbler/SrvIfaceMgr/SrvIfaceIface.cpp,v 1.11 2006-12-28 22:53:00 thomson Exp $
  *
  */
 
@@ -87,6 +87,7 @@ ostream & operator <<(ostream & strum, TSrvIfaceIface &x) {
     strum << "  <SrvIfaceIface";
     strum << " name=\"" << x.Name << "\"";
     strum << " ifindex=\"" << x.ID << "\"";
+    strum << " hwType=\"" << x.getHardwareType() << "\"";
     strum << " flags=\"" << x.Flags << "\">" << endl;
     
     if (x.Relay) {
