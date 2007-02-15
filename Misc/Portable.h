8e28f1bfbc8a7f95b99737fa684510902e444f69
Tomek Mrugalski <thomson@klub.com.pl>
2007-02-15 21:52:00 +0000

    *** empty log message ***

--- Misc/Portable.h
+++ Misc/Portable.h
@@ -8,14 +8,14 @@
  *
  * Released under GNU GPL v2 licence
  *
- * $Id: Portable.h,v 1.75 2007-02-03 19:07:01 thomson Exp $
+ * $Id: Portable.h,v 1.76 2007-02-15 21:52:00 thomson Exp $
  */	
 
 #ifndef PORTABLE_H
 #define PORTABLE_H
 
-/* #define DIBBLER_VERSION "0.5.0-CVS (" __DATE__ " " __TIME__ ")" */
-#define DIBBLER_VERSION "0.6.0-RC1"
+#define DIBBLER_VERSION "0.6.0-RC1-CVS (" __DATE__ " " __TIME__ ")"
+/* #define DIBBLER_VERSION "0.6.0-RC1" */
 
 #define DIBBLER_COPYRIGHT1 "| Dibbler - a portable DHCPv6, version " DIBBLER_VERSION
 #define DIBBLER_COPYRIGHT2 "| Authors : Tomasz Mrugalski<thomson(at)klub.com.pl>,Marek Senderski<msend(at)o2.pl>"
