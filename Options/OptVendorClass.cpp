22cb2ce4416114103159fcb3f926c7082d5156ac
Tomek Mrugalski <thomson@klub.com.pl>
2008-10-12 16:05:06 +0000

    OptVendorClass fields implemented.

--- Options/OptVendorClass.cpp
+++ Options/OptVendorClass.cpp
@@ -6,26 +6,101 @@
  *
  * released under GNU GPL v2 licence
  *
- * $Id: OptVendorClass.cpp,v 1.3 2005-07-17 19:56:55 thomson Exp $
- *
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2004/03/29 18:53:08  thomson
- * Author/Licence/cvs log/cvs version headers added.
- *
+ * $Id: OptVendorClass.cpp,v 1.4 2008-10-12 16:05:06 thomson Exp $
  *
  */
 #include "OptVendorClass.h"
+#include <iostream>
+#include <sstream>
+#include "Portable.h"
+#include "DHCPConst.h"
+#include "Logger.h"
+
 TOptVendorClass::TOptVendorClass( char * &buf,  int &n, TMsg* parent)	:TOpt(OPTION_VENDOR_CLASS, parent){
-}int TOptVendorClass::getSize(){	return 0;}
 
+    if (n<4) {
+	Log(Error) << "Unable to parse vendor-class info option." << LogEnd;
+	this->Vendor = 0;
+	this->VendorData = 0;
+	this->VendorDataLen = 0;
+	return;
+    }
 
+    this->Vendor = ntohl(*(int*)buf); // enterprise number
+    buf += 4;
+    n   -= 4;
 
- char * TOptVendorClass::storeSelf( char* buf)
+    if (!n) {
+	this->VendorData = 0;
+	this->VendorDataLen = 0;
+	return;
+    }
+
+    if (n) {
+	this->VendorData = new char[n];
+	memmove(this->VendorData, buf, n);
+    } else {
+	this->VendorData = 0;
+    }
+    this->VendorDataLen = n;
+
+    buf += n;
+    n    = 0;
+
+}
+
+int TOptVendorClass::getSize()
 {
-	return 0;
+    return 8+VendorDataLen; /* 8 normal header(4) + enterprise (4) */
+}
+
+/** 
+ * stores option in a buffer
+ * 
+ * @param buf option will be stored here
+ * 
+ * @return pointer to the next unused byte
+ */
+char * TOptVendorClass::storeSelf( char* buf)
+{
+    *(uint16_t*)buf = htons(OptType);
+    buf+=2;
+    *(uint16_t*)buf = htons( getSize()-4 );
+    buf+=2;
+    *(uint32_t*)buf = htonl(this->Vendor);
+    buf+=4;
+    memmove(buf, this->VendorData, this->VendorDataLen);
+    buf+=this->VendorDataLen;
+    return buf;    
 }
 
 bool TOptVendorClass::isValid()
 {
     return true;
 }
+
+int TOptVendorClass::getVendor()
+{
+    return this->Vendor;
+}
+
+char * TOptVendorClass::getVendorData()
+{
+    return this->VendorData;
+}
+
+string TOptVendorClass::getVendorDataPlain()
+{
+    ostringstream tmp;
+    tmp << "0x";
+    for (int i=0; i<this->VendorDataLen; i++) {
+	tmp << setfill('0') << setw(2) << hex << (unsigned int) this->VendorData[i];
+    }
+    return tmp.str();
+}
+
+int TOptVendorClass::getVendorDataLen()
+{
+    return this->VendorDataLen;
+}
+
--- Options/OptVendorClass.h
+++ Options/OptVendorClass.h
@@ -20,6 +20,17 @@ class TOptVendorClass : public TOpt
     int getSize();
     char * storeSelf( char* buf);
     bool isValid();
+
+    int getVendor();
+    char * getVendorData();      // returns vendor data (binary)
+    string getVendorDataPlain(); // returns vendor data (as a printable string)
+    int getVendorDataLen();      // returns vendor data length
+
+  protected:
+      int Vendor;
+      char * VendorData;
+      int VendorDataLen;
+
 };
 
 
