ed32bc90f7f59973dbc241e91536a2db746f0542
Tomek Mrugalski <thomson@klub.com.pl>
2009-03-24 23:32:29 +0000

    Patch from Liu Ming/BII group accepted (link state change)

---
+++ Port-linux/ethtool-local.h
@@ -0,0 +1,32 @@
+#ifndef fooethtoollocalhfoo
+#define fooethtoollocalhfoo
+
+/* $Id: ethtool-local.h,v 1.1 2009-03-24 23:32:29 thomson Exp $ */
+
+/*
+ * This file is part of ifplugd.
+ *
+ * ifplugd is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ifplugd is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ifplugd; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+typedef unsigned long long u64;
+typedef __uint32_t u32;        
+typedef __uint16_t u16;        
+typedef __uint8_t u8;          
+
+#include "ethtool-kernel.h"
+
+#endif
+
