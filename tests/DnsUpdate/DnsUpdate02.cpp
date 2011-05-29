3306bcbc36a5dd92a8f6d4cf8c143d9bd8644155
Tomek Mrugalski <thomson@klub.com.pl>
2011-05-29 18:29:53 +0000

    DNSUpdate unit tests changed.

--- tests/DnsUpdate/DnsUpdate02.cpp
+++ tests/DnsUpdate/DnsUpdate02.cpp
@@ -3,21 +3,23 @@
 #include "Logger.h"
 
 using namespace std;
-int main() {
+int main(int argc, const char *argv[]) {
 
-    char * dnsAddr   = "2000::1";
-    char * zonename  = "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.2.ip6.arpa.";
-    char * hostname  = "troi.example.com.";
-    char * hostip    = "2000::999";
-    int numOfRecords = 1; /* PTR */
+    string dnsAddr   = "2000::1";
+    string zonename  = "example.org.";
+    string hostname  = "troi.example.org.";
+    string hostip    = "2000::dead:beef";
 
-    DNSUpdate *act = new DNSUpdate(dnsAddr, zonename, hostname, hostip, "2h", numOfRecords);
+    DNSUpdate *act = new DNSUpdate(dnsAddr, zonename, hostname, hostip, 
+				   DNSUPDATE_AAAA, DNSUpdate::DNSUPDATE_TCP);
     int result = act->run();
     delete act;
     Log(Debug) << "RESULT: PTR=" << result << LogEnd;
 
-    DNSUpdate *act2= new DNSUpdate(dnsAddr, zonename, hostname, hostip, "2h", 2);
-    int result = act->run();
-    delete act;
+    DNSUpdate *act2= new DNSUpdate(dnsAddr, zonename, hostname, hostip,
+				   DNSUPDATE_AAAA, DNSUpdate::DNSUPDATE_UDP);
+    int result2 = act2->run();
+    delete act2;
+    Log(Debug) << "RESULT: PTR=" << result2 << LogEnd;
 
 }
