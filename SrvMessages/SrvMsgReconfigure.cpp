9be72f35ca23d498a28ceacae9181c7c2b8e054b
Tomek Mrugalski <thomson@klub.com.pl>
2011-09-09 21:21:45 +0200

    Cleanup in SrvMsgReconfigure

--- SrvMessages/SrvMsgReconfigure.cpp
+++ SrvMessages/SrvMsgReconfigure.cpp
@@ -6,7 +6,6 @@
  *
  * released under GNU GPL v2 only licence
  *
- * $Id: SrvMsgAdvertise.cpp,v 1.33 2008-11-13 22:40:26 thomson Exp $
  */
 
 #include "SrvMsgReconfigure.h"
@@ -27,53 +26,23 @@
 #include "SrvOptIA_PD.h"
 #include "Logger.h"
 
-TSrvMsgReconfigure::TSrvMsgReconfigure(int iface, SPtr<TIPv6Addr> clientAddr, SPtr<TIPv6Addr> ia ,int msgType, SPtr<TDUID> ptrDUID)
+TSrvMsgReconfigure::TSrvMsgReconfigure(int iface, SPtr<TIPv6Addr> clientAddr,
+                                       SPtr<TIPv6Addr> ia ,int msgType,
+                                       SPtr<TDUID> ptrDUID)
     :TSrvMsg(iface, clientAddr, RECONFIGURE_MSG, rand()%(2^24) )
 {
-    //getORO( (Ptr*)solicit );
-    //copyClientID( (Ptr*)solicit );
-    //copyRelayInfo( clientAddr );
-    //copyAAASPI( (Ptr*)solicit );
-    //copyRemoteID( (Ptr*)solicit );
-ClientDUID=ptrDUID;
+    ClientDUID=ptrDUID;
 
-    // append serverID, preference and possibly unicast
-    //appendMandatoryOptions(ORO);
+    // status code is not necessary in Reconfigure
+    //appendStatusCode();
 
-    //if client requested parameters and policy doesn't forbid from answering
-    //appendRequestedOptions(ClientDUID, clientAddr, Iface, ORO);
-
-
-    appendStatusCode();
-
-//SPtr<TIPv6Addr> clntAddr = PeerAddr;
-
-    // this is ADVERTISE only, so we need to release assigned addresses
-    if(msgType==1) {
-Log(Crit) << " wwwwwwwwwww " << PeerAddr->getPlain() << LogEnd;
-
-            //ptrOptIA_NA->releaseAllAddrs(true);
-//Options.push_back(new TOptInteger(OPTION_ADDRPARAMS, 2, 0, this));
-Log(Crit) << " jestem_na_warunku_msgType=1 " << LogEnd;
-            //break;
-        }
-
-        else {
-            SPtr<TSrvOptTA> ta;
-            //ta = (Ptr*) opt;
-            //ta->releaseAllAddrs(false);
-            //break;
-        }
-        //default:
-        //    break;
-
+    /// @todo Add necessary options here
+    /// see RFC3315, section 15.11
 
     appendAuthenticationOption(ClientDUID);
 
-   // pkt = new char[this->getSize()];
-   // MRT = 0;
-Log(Crit) << "przed wysylka " << LogEnd;
-    //send();
+    pkt = new char[this->getSize()];
+    send();
     return;
 }
 
--- SrvMessages/SrvMsgReconfigure.h
+++ SrvMessages/SrvMsgReconfigure.h
@@ -17,11 +17,10 @@ class TSrvMsgReconfigure : public TSrvMsg
 {
   public:
     // creates object based on a buffer
-    TSrvMsgReconfigure(int iface, SPtr<TIPv6Addr> addr, SPtr<TIPv6Addr> ia, int msgType, SPtr<TDUID> ptrDUID);
-    /// @todo: get rid of 2 of those constructors
+    TSrvMsgReconfigure(int iface, SPtr<TIPv6Addr> addr, SPtr<TIPv6Addr> ia,
+                       int msgType, SPtr<TDUID> ptrDUID);
 
     bool check();
-    //bool handleSolicitOptions(SPtr<TSrvMsgSolicit> solicit);
     void doDuties();
     unsigned long getTimeout();
     string getName();
