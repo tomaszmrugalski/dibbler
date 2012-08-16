f8a44d264134c64ce2d914b9e613ea9600d16759
Tomek Mrugalski <thomson@klub.com.pl>
2012-08-16 22:32:36 +0200

    DnsUpdate tests should now pass, regardless if there is previous entry in DNS

--- IfaceMgr/tests/DnsUpdate_unittest.cc
+++ IfaceMgr/tests/DnsUpdate_unittest.cc
@@ -27,7 +27,7 @@ public:
         DNSUpdate::sendMsg(timeout);
     }
 
-    void extractSignature() {
+    void extractPacket() {
         DnsRR * tsig = Message_->tsig_rr;
         if (tsig) {
             if (tsig->presign_RDLENGTH) {
@@ -54,16 +54,26 @@ TEST(DnsUpdateTest, AAAA) {
 
     unsigned int timeout = 1000;
 
-    message_buff expected;
+    message_buff expected1;
+    message_buff expected2;
     // hexstring exported from wireshark
+
+    // this also includes case where there is old AAAA record and DNS server responded
+    // this version also includes delete of the old AAAA record
+    hexToBin("a09328000001000000020000076578616d706c65036f7267000006000103666f6"
+             "fc00c001c00fe00000000001020010000000000000000000000000001c01d001c"
+             "000100001c20001020010000000000000000000000000001", expected1);
+
+    // simple add (no previous AAAA records)
     hexToBin("a09328000001000000010000076578616d706c65036f7267000006000103666f6"
-             "fc00c001c000100001c20001020010000000000000000000000000001", expected);
+    "fc00c001c000100001c20001020010000000000000000000000000001", expected2);
 
     act.id_ = 0xa093; // just a random Transaction ID
 
     result = act.run(timeout);
+    act.extractPacket();
 
-    EXPECT_TRUE(cmpBuffers(expected, act.buffer_));
+    EXPECT_TRUE(cmpBuffers(expected1, act.buffer_) || cmpBuffers(expected2, act.buffer_));
 
     act.showResult(result);
 }
@@ -81,13 +91,23 @@ TEST(DnsUpdateTest, TSIG_AAAA) {
 
     act.setTSIG("DDNS_KEY", key.getPackedData(), "HMAC-MD5.SIG-ALG.REG.INT", 301 /* fudge */);
 
-    message_buff expected;
+    message_buff expected1;
+    message_buff expected2;
+    // this also includes case where there is old AAAA record and DNS server responded
+    // this version also includes delete of the old AAAA record
     hexToBin("a09428000001000000020001076578616d706c65036f7267000006000103666f6"
              "fc00c001c00fe00000000001020010000000000000000000000000001c01d001c"
              "000100001c200010200100000000000000000000000000010844444e535f4b455"
              "90000fa00ff00000000003a08484d41432d4d4435075349472d414c4703524547"
              "03494e54000000500dbd7a012d00106f9d86b83c867043dd14f047bf7014fda09"
-             "400000000", expected);
+             "400000000", expected1);
+
+    // simple add (no previous AAAA records)
+    hexToBin("a09428000001000000010001076578616d706c65036f7267000006000103666f6"
+             "fc00c001c000100001c200010200100000000000000000000000000010844444e"
+             "535f4b45590000fa00ff00000000003a08484d41432d4d4435075349472d414c4"
+             "70352454703494e54000000500dbd7a012d0010ef1e84d5fb49f5305faa73dd7c"
+             "0e1e15a09400000000", expected2);
 
     act.id_ = 0xa094; // just a random Transaction ID
     act.sign_time_ = 0x500dbd7a;
@@ -95,13 +115,13 @@ TEST(DnsUpdateTest, TSIG_AAAA) {
     result = act.run(timeout);
 
     // build message and store it in act.buffer_
-    act.extractSignature();
-    EXPECT_TRUE(cmpBuffers(expected, act.buffer_));
+    act.extractPacket();
+    EXPECT_TRUE(cmpBuffers(expected1, act.buffer_) || cmpBuffers(expected2, act.buffer_));
 
     // call it second time to verify that we can build the same message
     // multiple times (test environment sanity check)
-    act.extractSignature();
-    EXPECT_TRUE(cmpBuffers(expected, act.buffer_));
+    act.extractPacket();
+    EXPECT_TRUE(cmpBuffers(expected, act.buffer_) || cmpBuffers(expected2, act.buffer_));
 
     act.showResult(result);
 }
