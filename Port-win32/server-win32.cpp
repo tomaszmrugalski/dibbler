1a94514f795295223488af94e94f315d265274d5
Tomek Mrugalski <thomson@klub.com.pl>
2012-08-20 00:20:06 +0200

    Windows code indent fixes.

--- Port-win32/server-win32.cpp
+++ Port-win32/server-win32.cpp
@@ -11,7 +11,7 @@
 #include <string>
 #include <iostream>
 #include <winsock2.h>
-#include <Ws2tcpip.h> 
+#include <Ws2tcpip.h>
 
 #include "Portable.h"
 #include "DHCPServer.h"
@@ -26,110 +26,108 @@ extern TDHCPServer * srvPtr;
 using namespace std;
 
 void usage() {
-	cout << "Usage:" << endl;
-	cout << " dibbler-server.exe ACTION [-d c:\\path\\to\\config\\file]" << endl
-		 << " ACTION = status|start|stop|install|uninstall|run" << endl
-		 << " status    - show status and exit" << endl
-		 << " start     - start installed service" << endl
-		 << " stop      - stop installed service" << endl
-		 << " install   - install service" << endl
-		 << " uninstall - uninstall service" << endl
-		 << " run       - run in console" << endl << endl 
-		 << " -d paramters is now optional." << endl;
+        cout << "Usage:" << endl;
+        cout << " dibbler-server.exe ACTION [-d c:\\path\\to\\config\\file]" << endl
+                 << " ACTION = status|start|stop|install|uninstall|run" << endl
+                 << " status    - show status and exit" << endl
+                 << " start     - start installed service" << endl
+                 << " stop      - stop installed service" << endl
+                 << " install   - install service" << endl
+                 << " uninstall - uninstall service" << endl
+                 << " run       - run in console" << endl << endl
+                 << " -d paramters is now optional." << endl;
 
 }
 
-/* 
- * Handle the CTRL-C, CTRL-BREAK signal. 
+/*
+ * Handle the CTRL-C, CTRL-BREAK signal.
  */
-BOOL CtrlHandler( DWORD fdwCtrlType ) 
-{ 
-  switch( fdwCtrlType ) 
-  { 
+BOOL CtrlHandler( DWORD fdwCtrlType )
+{
+  switch( fdwCtrlType )
+  {
   case CTRL_C_EVENT: {
-	  srvPtr->stop();
+          srvPtr->stop();
       return TRUE;
   }
-    case CTRL_BREAK_EVENT: 
-      return FALSE; 
+    case CTRL_BREAK_EVENT:
+      return FALSE;
   }
   return TRUE;
 }
 
 int main(int argc, char* argv[]) {
-    
+
     cout << DIBBLER_COPYRIGHT1 << " (SERVER, WinXP/2003/Vista/Win7 port)" << endl;
     cout << DIBBLER_COPYRIGHT2 << endl;
     cout << DIBBLER_COPYRIGHT3 << endl;
     cout << DIBBLER_COPYRIGHT4 << endl;
     cout << endl;
 
-	// get the service object
+        // get the service object
     TSrvService * SrvService = TSrvService::getHandle();
 
-	int i=0;
     WSADATA wsaData;
     if( WSAStartup( MAKEWORD( 2, 2 ), &wsaData )) {
-	cout<<"Unable to load WinSock 2.2"<<endl;
-	return 0;
+        cout<<"Unable to load WinSock 2.2"<<endl;
+        return 0;
     }
-    
+
     // find ipv6.exe (or netsh.exe in future implementations)
     if (!lowlevelInit()) {
-	cout << "lowlevelInit() failed. Startup aborted.\n";
-	return -1;		
+        cout << "lowlevelInit() failed. Startup aborted.\n";
+        return -1;
     }
-    
+
     EServiceState status = SrvService->ParseStandardArgs(argc, argv);
     SrvService->setState(status);
 
     // is this proper port?
-	if (!SrvService->verifyPort()) {
-       Log(Crit) << "Operating system version is not supported by this Dibbler port." << LogEnd;
+    if (!SrvService->verifyPort()) {
+        Log(Crit) << "Operating system version is not supported by this Dibbler port."
+                  << LogEnd;
        return -1;
     }
-    
+
     SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
-    
+
     switch (status) {
     case STATUS: {
-	    SrvService->showStatus();
-    	break;
+        SrvService->showStatus();
+        break;
     };
     case START: {
-	    SrvService->StartService();
-	    break;
+        SrvService->StartService();
+        break;
     }
     case STOP: {
         SrvService->StopService();
-	    break;
+        break;
     }
     case INSTALL: {
-	    SrvService->Install();
-	    break;
+        SrvService->Install();
+        break;
     }
     case UNINSTALL: {
-	    SrvService->Uninstall();
-	    break;
-    } 
+        SrvService->Uninstall();
+        break;
+    }
     case RUN: {
-	    SrvService->Run();
-	    break;
+        SrvService->Run();
+        break;
     }
     case SERVICE: {
-	    SrvService->RunService();
-	    break;
+        SrvService->RunService();
+        break;
     }
     case INVALID: {
-    	cout << "Invalid usage." << endl;
-    }				  
-    case HELP: 
-    default: {
-	    usage();
-    }	
+        cout << "Invalid usage." << endl;
     }
-    
+    case HELP:
+    default: {
+        usage();
+    }
+    }
+
     return 0;
 }
-
-
