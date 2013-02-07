229c98983ea0ed225e91a580b6b1e9cfbae9f4fc
Tomek Mrugalski <thomson@klub.com.pl>
2013-02-07 14:52:38 +0100

    Compilation fix for --disable-auth

--- SrvOptions/SrvOptKeyGeneration.h
+++ SrvOptions/SrvOptKeyGeneration.h
@@ -5,17 +5,12 @@
  *
  * released under GNU GPL v2 only licence
  *
- * $Id: SrvOptKeyGeneration.h,v 1.2 2008-08-29 00:07:36 thomson Exp $
- *
- * $Log: not supported by cvs2svn $
- * Revision 1.1  2008-02-25 20:42:46  thomson
- * Missing new AUTH files added.
- *
- *
  */
 
 #ifndef SRVOPTKEYGENERATION_H
 #define SRVOPTKEYGENERATION_H
+
+#ifndef MOD_DISABLE_AUTH
 
 #include "OptKeyGeneration.h"
 #include "SrvMsg.h"
@@ -28,5 +23,7 @@ class TSrvOptKeyGeneration : public TOptKeyGeneration
     TSrvOptKeyGeneration(TSrvMsg* parent);
     bool doDuties();
 };
+
+#endif
 
 #endif
