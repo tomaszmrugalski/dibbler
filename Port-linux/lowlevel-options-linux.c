c4d9d35d2b6e9a3893c986d2b19309658abb3103
Tomek Mrugalski <thomson@klub.com.pl>
2017-06-23 21:04:50 +0200

    whitespace cleanup, error_message() now used safely.

--- Port-linux/lowlevel-options-linux.c
+++ Port-linux/lowlevel-options-linux.c
@@ -29,15 +29,11 @@
 #define LF 0x0d
 #define MAX_LINE_LEN 511
 
-
-extern char * Message;
-
-
 #ifdef MOD_RESOLVCONF
 /** @brief check whether the resolconf executable exists
  *
  * Tries to spawn resolvconf process and returns a pipe to it.
- * Parameters are passed to resolvconf as command line 
+ * Parameters are passed to resolvconf as command line
  * arguments (e.g. -a|-d, "IFNAME")
  *
  * the pipe needs to be closed by the caller
@@ -59,18 +55,18 @@ FILE *resolvconf_open(const char *arg1, const char *arg2)
     switch(child = fork()) {
       case 0: /* child */
           close(pipefd[1]);
-	  close(0);
-	  dup(pipefd[0]);
-	  close(pipefd[0]);
-	  /* double fork so init reaps the child */
-	  if (!fork()) { /* child */
+          close(0);
+          dup(pipefd[0]);
+          close(pipefd[0]);
+          /* double fork so init reaps the child */
+          if (!fork()) { /* child */
               execl(RESOLVCONF, RESOLVCONF, arg1, arg2, (char *)NULL);
-	  } /* All other cases are meaningless here */
-	  exit(EXIT_FAILURE);
-	  break;
+          } /* All other cases are meaningless here */
+          exit(EXIT_FAILURE);
+          break;
     case EXIT_FAILURE: /* error */
           return NULL;
-	  break;
+          break;
     }
     /* parent */
     close(pipefd[0]);
@@ -84,79 +80,79 @@ extern int iproute_modify(int cmd, unsigned flags, int argc, char **argv);
 
 /**
  * @brief Removes an entry from a file.
- * 
+ *
  * Remove value of keyword from opened file in and the result is printed into
  * opened file out. If removed_empty and keyword remains without argument, it
  * will be removed too. Comments (starting with comment char) are respected.
  * All values following keyword are removed on all lines (global remove).
- * 
- * @param in 
- * @param out 
- * @param keyword 
- * @param value 
- * @param comment 
- * @param remove_empty 
- * 
+ *
+ * @param in
+ * @param out
+ * @param keyword
+ * @param value
+ * @param comment
+ * @param remove_empty
+ *
  * @return Returns LOWLEVEL_NO_ERROR by default, LOWLEVEL_ERROR_FILE on I/O error \
  *         (and errno is set up), LOWLEVEL_ERROR_UNSPEC if value or keyword is too long.
  */
 int cfg_value_del(FILE *in, FILE *out, const char *keyword, const char *value,
-	const char comment, int remove_empty) {
+        const char comment, int remove_empty) {
     char buf[MAX_LINE_LEN+1];
 
     if (strlen(keyword) > MAX_LINE_LEN || strlen(value) > MAX_LINE_LEN)
-	return(LOWLEVEL_ERROR_UNSPEC);
+        return(LOWLEVEL_ERROR_UNSPEC);
 
     errno = 0;
     while (fgets(buf, MAX_LINE_LEN, in)) {
 
-	char *head;
-	/* Skip leading white space */
-	for (head=buf; *head!='\0' && isspace((int)(*head)); head++);
-	/* Skip comment */
-	if (*head!='\0' && *head!=comment) {
-	    /* Find keyword */
-	    if (strstr(head, keyword)==head) {
-		head += strlen(keyword);
+        char *head;
+        /* Skip leading white space */
+        for (head=buf; *head!='\0' && isspace((int)(*head)); head++);
+        /* Skip comment */
+        if (*head!='\0' && *head!=comment) {
+            /* Find keyword */
+            if (strstr(head, keyword)==head) {
+                head += strlen(keyword);
 
-		/* Skip alone keyword */
-		if (*head!='\0' && *head!=comment && isspace((int)(*head))) {
-		    char *keyword_end=head;
+                /* Skip alone keyword */
+                if (*head!='\0' && *head!=comment && isspace((int)(*head))) {
+                    char *keyword_end=head;
 
-		    /* Locate each argument */
-		    while (*head!='\0' && *head!=comment) {
-		    char *argument_begin=head;
-		    /* Skip spaces before argument */
-		    for (; *head!='\0' && isspace((int)(*head)); head++);
-		    if (*head!='\0' && *head!=comment) {
-			/* Compare argument to value */
-			if (strstr(head, value)==head) {
-			    head += strlen(value);
-			    if (*head=='\0' || *head==comment ||
-				    isspace((int)(*head))) {
-				/* remove this argument */
-				/* sprinf() moves data and appends '\0' */
-				sprintf(argument_begin, "%s", head);
-				head=argument_begin;
-			    }
-			}
-			/* Skip rest of the argument */
-			for (; *head!='\0' && !isspace((int)(*head)); head++);
-		    }
-		}
+                    /* Locate each argument */
+                    while (*head!='\0' && *head!=comment) {
+                    char *argument_begin=head;
+                    /* Skip spaces before argument */
+                    for (; *head!='\0' && isspace((int)(*head)); head++);
+                    if (*head!='\0' && *head!=comment) {
+                        /* Compare argument to value */
+                        if (strstr(head, value)==head) {
+                            head += strlen(value);
+                            if (*head=='\0' || *head==comment ||
+                                    isspace((int)(*head))) {
+                                /* remove this argument */
+                                /* sprinf() moves data and appends '\0' */
+                                sprintf(argument_begin, "%s", head);
+                                head=argument_begin;
+                            }
+                        }
+                        /* Skip rest of the argument */
+                        for (; *head!='\0' && !isspace((int)(*head)); head++);
+                    }
+                }
 
-		/* Remove whole line if keyword remains without any arguments.*/
-		if (remove_empty) {
-		    for (head=keyword_end; *head!='\0' && isspace((int)(*head));
-			    head++);
-		    if (*head=='\0' || *head==comment) *buf='\0';
-		    }
-		}
-	    }
-	}
+                /* Remove whole line if keyword remains without any arguments.*/
+                if (remove_empty) {
+                    for (head=keyword_end; *head!='\0' && isspace((int)(*head));
+                            head++);
+                    if (*head=='\0' || *head==comment) *buf='\0';
+                    }
+                }
+            }
+        }
 
-	/* print output */
-	if (-1 == fprintf(out, "%s", buf)) return(LOWLEVEL_ERROR_FILE);
+        /* print output */
+        if (-1 == fprintf(out, "%s", buf)) return(LOWLEVEL_ERROR_FILE);
     }
 
     return((errno)?LOWLEVEL_ERROR_FILE:LOWLEVEL_NO_ERROR);
@@ -184,22 +180,22 @@ int cfg_file_del(const char *file, const char *keyword, const char *value) {
 
     /* Create temporary FILE */
     if ((tmpfd = mkstemp(template)) == -1)
-	return (LOWLEVEL_ERROR_FILE);
+        return (LOWLEVEL_ERROR_FILE);
 
     if (!(ftmp = fdopen(tmpfd, "w"))) {
         /* close the file first, then... */
-	close(tmpfd);
+        close(tmpfd);
 
         /* then delete it */
-	unlink(template);
-	return(LOWLEVEL_ERROR_FILE);
+        unlink(template);
+        return(LOWLEVEL_ERROR_FILE);
     }
 
     /* Open original file */
     if (!(fold = fopen(file, "r"))) {
-	unlink(template);
-	fclose(ftmp);
-	return(LOWLEVEL_ERROR_FILE);
+        unlink(template);
+        fclose(ftmp);
+        return(LOWLEVEL_ERROR_FILE);
     }
 
     /* modify configuration */
@@ -216,10 +212,10 @@ int cfg_file_del(const char *file, const char *keyword, const char *value) {
 
     /* move temp file into place of the old one */
     if (error==LOWLEVEL_NO_ERROR) {
-	memset(&st,0,sizeof(st));
-	if (stat(file, &st) || rename(template, file) ||
-		chmod(file, st.st_mode))
-	    error=LOWLEVEL_ERROR_FILE;
+        memset(&st,0,sizeof(st));
+        if (stat(file, &st) || rename(template, file) ||
+                chmod(file, st.st_mode))
+            error=LOWLEVEL_ERROR_FILE;
     }
 
     return(error);
@@ -242,9 +238,12 @@ int dns_add(const char * ifname, int ifaceid, const char * addrPlain) {
 
     /* if resolvconf is not available, fallback to normal file append */
     if (!f && !(f=fopen(RESOLVCONF_FILE, "a+")) ) {
-            return LOWLEVEL_ERROR_FILE;
+        sprintf(error_message(), "Unable to open %s file:%s",
+                RESOLVCONF_FILE, strerror(errno));
+
+        return LOWLEVEL_ERROR_FILE;
     }
-    
+
     fseek(f, -1, SEEK_END);
     c = fgetc(f); /* read the last character */
 
@@ -259,7 +258,7 @@ int dns_add(const char * ifname, int ifaceid, const char * addrPlain) {
 }
 
 int dns_del(const char * ifname, int ifaceid, const char *addrPlain) {
-    
+
 #ifdef MOD_RESOLVCONF
     FILE *f = NULL;
     /* try to use resolvconf to remove config */
@@ -268,7 +267,7 @@ int dns_del(const char * ifname, int ifaceid, const char *addrPlain) {
         return LOWLEVEL_NO_ERROR;
     }
 #endif
-    
+
     return cfg_file_del(RESOLVCONF_FILE, "nameserver", addrPlain);
 }
 
@@ -287,7 +286,7 @@ int domain_add(const char* ifname, int ifaceid, const char* domain) {
         return LOWLEVEL_NO_ERROR;
     }
 #endif
-    
+
     /* otherwise do the edit on your own */
 
     memset(&st,0,sizeof(st));
@@ -296,31 +295,31 @@ int domain_add(const char* ifname, int ifaceid, const char* domain) {
     unlink(RESOLVCONF_FILE ".old");
     rename(RESOLVCONF_FILE, RESOLVCONF_FILE ".old");
     if ( !(f = fopen(RESOLVCONF_FILE ".old", "r")) )
-	return LOWLEVEL_ERROR_FILE;
+        return LOWLEVEL_ERROR_FILE;
     if ( !(f2 = fopen(RESOLVCONF_FILE, "w+")) ) {
         fclose(f);
-	return LOWLEVEL_ERROR_FILE;
+        return LOWLEVEL_ERROR_FILE;
     }
     while (fgets(buf,511,f)) {
-	if ( (!found) && (strstr(buf, "search")) ) {
-	    if (strlen(buf))
-		buf[strlen(buf)-1]=0;
-	    fprintf(f2, "%s %s\n", buf, domain);
-	    found = 1;
-	    continue;
-	}
-	fprintf(f2,"%s",buf);
+        if ( (!found) && (strstr(buf, "search")) ) {
+            if (strlen(buf))
+                buf[strlen(buf)-1]=0;
+            fprintf(f2, "%s %s\n", buf, domain);
+            found = 1;
+            continue;
+        }
+        fprintf(f2,"%s",buf);
     }
 
     fseek(f2, -1, SEEK_END);
     c = fgetc(f2);
     fseek(f2,0, SEEK_END);
     if ( (c!=CR) && (c!=LF) ) {
-	fprintf(f2,"\n");
+        fprintf(f2,"\n");
     }
 
     if (!found)
-	fprintf(f2,"search %s\n",domain);
+        fprintf(f2,"search %s\n",domain);
 
     fclose(f);
     fclose(f2);
@@ -348,14 +347,14 @@ int ntp_add(const char* ifname, const int ifindex, const char* addrPlain){
     FILE * f;
     unsigned char c;
     if ( !(f=fopen(NTPCONF_FILE,"a+"))) {
-	return LOWLEVEL_ERROR_FILE;
+        return LOWLEVEL_ERROR_FILE;
     }
 
     fseek(f, -1, SEEK_END);
     c = fgetc(f);
     fseek(f,0, SEEK_END);
     if ( (c!=CR) && (c!=LF) ) {
-	fprintf(f,"\n");
+        fprintf(f,"\n");
     }
 
     fprintf(f,"server %s\n",addrPlain);
@@ -367,7 +366,7 @@ int ntp_del(const char* ifname, const int ifindex, const char* addrPlain){
     return cfg_file_del(NTPCONF_FILE, "server", addrPlain);
 }
 
-/* 
+/*
  * Set new timezone by making symlink (usually from /etc/localtime to
  * /usr/share/zoneinfo/<timezone>).
  * Only symbolic abberviated timezone specification is assumed. POSIX
@@ -377,25 +376,25 @@ int timezone_set(const char* ifname, int ifindex, const char* timezone){
     /* timezone-data README states:
      * file name component must not exceed 14 characters
      * file name components use only ASCII letters, '.', '-' and '_'.
-     * Thus TZ_LEN >= 
+     * Thus TZ_LEN >=
      * strlen(TIMEZONES_DIR) + '/' + file name component1 + '/' + component2 */
 #define TZ_LEN 64
 #define TIMEZONE_FILE_TMP TIMEZONE_FILE".dibbler"
     char buf[TZ_LEN];
     struct stat st;
-    
+
     if (!timezone || strlen(timezone)==0) return LOWLEVEL_ERROR_UNSPEC;
     /* Security check: Do not allow evil server to traverse client file system */
     if (strstr(timezone, "..")) return LOWLEVEL_ERROR_UNSPEC;
     if (TZ_LEN <= snprintf(buf, TZ_LEN, "%s/%s", TIMEZONES_DIR, timezone))
-	return LOWLEVEL_ERROR_UNSPEC;
+        return LOWLEVEL_ERROR_UNSPEC;
     if (stat(buf, &st) || S_ISDIR(st.st_mode)) return LOWLEVEL_ERROR_FILE;
 
     if (unlink(TIMEZONE_FILE_TMP) && errno!=ENOENT) return LOWLEVEL_ERROR_FILE;
     if (symlink(buf, TIMEZONE_FILE_TMP)) return LOWLEVEL_ERROR_FILE;
     if (rename(TIMEZONE_FILE_TMP, TIMEZONE_FILE)) {
-	unlink(TIMEZONE_FILE_TMP);
-	return LOWLEVEL_ERROR_FILE;
+        unlink(TIMEZONE_FILE_TMP);
+        return LOWLEVEL_ERROR_FILE;
     }
     return LOWLEVEL_NO_ERROR;
 }
@@ -454,23 +453,22 @@ int nisplusdomain_del(const char* ifname, int ifindex, const char* domain){
 
 void add_radvd_conf(const char* ifname, const char* prefixPlain, int prefixLength,
                     uint32_t preferred, uint32_t valid) {
-    char * errorMsg = error_message();
-
     FILE * f;
     f = fopen(RADVD_FILE, "r+");
     if (!f) {
-	/* unable to open, so this file is missing, let's create it */
-	f = fopen(RADVD_FILE, "w");
-	fprintf(f, "#\n");
-	fprintf(f, "# Router Advertisement config file generated by Dibbler %s\n", DIBBLER_VERSION);
-	fprintf(f, "#\n");
-	fprintf(f, "\n");
+        /* unable to open, so this file is missing, let's create it */
+        f = fopen(RADVD_FILE, "w");
+        fprintf(f, "#\n");
+        fprintf(f, "# Router Advertisement config file generated by Dibbler %s\n", DIBBLER_VERSION);
+        fprintf(f, "#\n");
+        fprintf(f, "\n");
     }
     if (!f) {
-	sprintf(errorMsg, "Unable to open %s file.", RADVD_FILE);
+        sprintf(error_message(), "Unable to open %s file.", RADVD_FILE);
+        return;
     }
     fseek(f, 0, SEEK_END);
-    
+
     fprintf(f, "\n### %s start ###\n", ifname);
     fprintf(f, "interface %s \n", ifname);
     fprintf(f, "{ \n");
@@ -503,22 +501,22 @@ void delete_radvd_conf(const char* ifname, const char* prefixPlain, int prefixLe
     rename(RADVD_FILE,RADVD_FILE".old");
 
     f = fopen(RADVD_FILE".old","r");
-    f2 = fopen(RADVD_FILE,"w"); 
+    f2 = fopen(RADVD_FILE,"w");
 
     snprintf(buf2, 511, "### %s start ###\n", ifname);
     while (fgets(buf,511,f)) {
-	if ( (!found) && (strstr(buf, buf2)) ) {
-	    found = 1;
-	    snprintf(buf2, 511, "### %s end ###\n", ifname);
-	    continue;
-	}
-	if ( (found) && (strstr(buf, buf2)) ) {
-	    found = 0;
-	    continue;
-	}
+        if ( (!found) && (strstr(buf, buf2)) ) {
+            found = 1;
+            snprintf(buf2, 511, "### %s end ###\n", ifname);
+            continue;
+        }
+        if ( (found) && (strstr(buf, buf2)) ) {
+            found = 0;
+            continue;
+        }
 
-	if (!found)
-	    fprintf(f2,"%s",buf);
+        if (!found)
+            fprintf(f2,"%s",buf);
     }
     fclose(f);
     fclose(f2);
@@ -540,7 +538,7 @@ void delete_radvd_conf(const char* ifname, const char* prefixPlain, int prefixLe
  * @return negative error code or 0 if successful
  */
 int prefix_add(const char* ifname, int ifindex, const char* prefixPlain, int prefixLength,
-	       unsigned long preferred, unsigned long valid)
+               unsigned long preferred, unsigned long valid)
 {
     char *argv[5];
     int result;
@@ -566,12 +564,14 @@ int prefix_add(const char* ifname, int ifindex, const char* prefixPlain, int pre
 
     if (result == 0)
         return LOWLEVEL_NO_ERROR;
-    else
+    else {
+        sprintf(error_message(), "Unable to add prefix: %s", strerror(errno) );
         return LOWLEVEL_ERROR_UNSPEC;
+    }
 }
 
 int prefix_update(const char* ifname, int ifindex, const char* prefixPlain, int prefixLength,
-		  unsigned long prefered, unsigned long valid)
+                  unsigned long prefered, unsigned long valid)
 {
     char *argv[3];
     int result;
@@ -594,8 +594,10 @@ int prefix_update(const char* ifname, int ifindex, const char* prefixPlain, int 
 
     if (result == 0)
         return LOWLEVEL_NO_ERROR;
-    else
+    else {
+        sprintf(error_message(), "Unable to update prefix: %s", strerror(errno) );
         return LOWLEVEL_ERROR_UNSPEC;
+    }
 }
 
 
@@ -611,11 +613,13 @@ int prefix_del(const char* ifname, int ifindex, const char* prefixPlain, int pre
     argv[0] = buf;
     argv[1] = "dev";
     argv[2] = (char*)ifname;
-    
+
     result = iproute_modify(RTM_DELROUTE, 0, 3, argv);
 
     if (result == 0)
         return LOWLEVEL_NO_ERROR;
-    else
+    else {
+        sprintf(error_message(), "Unable to delete prefix: %s", strerror(errno));
         return LOWLEVEL_ERROR_UNSPEC;
+    }
 }
