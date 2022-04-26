ff96cc4c165469b099463119093523877fc7c11e
Matthieu Patou <mat@matws.net>
2022-04-26 08:30:03 -0700

    Fix calling parameters for TOptIAPrefix
    prefix length and validity were swapped causing wrong prefix length to
    be requested (often 255) during prefix delegation in SOLICIT messages.
    
    Signed-off-by: Matthieu Patou <mat@matws.net>

--- ClntOptions/ClntOptIA_PD.cpp
+++ ClntOptions/ClntOptIA_PD.cpp
@@ -59,8 +59,8 @@ TClntOptIA_PD::TClntOptIA_PD(SPtr<TClntCfgPD> cfgPD, TMsg *parent)
   cfgPD->firstPrefix();
   SPtr<TClntCfgPrefix> cfgPrefix;
   while (cfgPrefix = cfgPD->getPrefix()) {
-    SubOptions.append(new TOptIAPrefix(cfgPrefix->get(), cfgPrefix->getPref(),
-                                       cfgPrefix->getValid(), cfgPrefix->getLength(), 0));
+    SubOptions.append(new TOptIAPrefix(cfgPrefix->get(), cfgPrefix->getLength(),
+                                       cfgPrefix->getPref(), cfgPrefix->getValid(), 0));
   }
   clearContext();
 }
